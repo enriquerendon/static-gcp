850810f9de73ce10c2953999ec7b40d4bb39b9f7
Enrique Rendon <enrique.rendon@westernacher.com>
2018-12-27 01:05:31 +0000

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,11 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function (Controller) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-    onShowHello : function () {
-       // show a native JavaScript alert
-       alert("Hello World");
-    }
- });
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+		onShowHello: function () {
+			MessageToast.show("Hello World");
+		}
+	});
 });
